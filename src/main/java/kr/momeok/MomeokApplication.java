9e8d50865d090f8f4e759c2eb9560d81f7345b7b
infreeJ <kasious97@naver.com>
2026-02-25 20:47:25 +0900

    init

---
+++ src/main/java/kr/momeok/MomeokApplication.java
@@ -0,0 +1,13 @@
+package kr.momeok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomeokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomeokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/momeok/MomeokApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.momeok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomeokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
